af219c4b67b0e81b67b3590e61724fa0b736701c
Sarthak Singhal <47398967+sarthakcyberpro@users.noreply.github.com>
2022-06-21 09:07:09 +0530

    Update itemRepository.java

--- Inventory/src/main/java/com/example/Inventory/itemRepository.java
+++ Inventory/src/main/java/com/example/Inventory/itemRepository.java
@@ -30,43 +30,4 @@ public class itemRepository {
         }
         return null;
     }
-
-    public List<items> search(String name) {
-        return list.stream().filter(x -> x.getName().startsWith(name)).collect(Collectors.toList());
-    }
-
-    public items save(items p) {
-        items item = new items();
-        item.setId(p.getId());
-        item.setName(p.getName());
-        item.setQuantity(p.getQuantity());
-        item.setPrice(p.getPrice());
-        list.add(item);
-        return item;
-    }
-
-    public String delete(Integer id) {
-        list.removeIf(x -> x.getId() == (id));
-        return null;
-    }
-
-    public items update(items item) {
-        int idx = 0;
-        int id = 0;
-        for (int i = 0; i < list.size(); i++) {
-            if (list.get(i).getId() == (item.getId())) {
-                id = item.getId();
-                idx = i;
-                break;
-            }
-        }
-
-        items item1 = new items();
-        item1.setId(id);
-        item1.setName(item.getName());
-        item1.setQuantity(item.getQuantity());
-        item1.setPrice(item.getPrice());
-        list.set(idx, item);
-        return item1;
-    }
 }
